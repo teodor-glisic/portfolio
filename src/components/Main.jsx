74ba02f7a1e8a147a8b8a43ae958735d47ba2554
Teodor Glisic <teodor.glisic13@icloud.com>
2025-04-02 00:00:58 +0200

    Updated Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,24 +2,11 @@ export default function Main() {
     return (
         <>
             <main className="main-container">
-                <h1>Main Page</h1>
-
+                
+                
                 <img src="../portfolio/portrait.jpg" id="portrait" alt="Portrait of Teodor Glisic"></img>
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magni adipisci. Provident quia dignissimos delectus vero sapiente soluta. Nihil laboriosam hic dolor nostrum cupiditate sunt deserunt autem nam aperiam ducimus?</p>
+                
 
             </main>
         </>
